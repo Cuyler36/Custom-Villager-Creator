c33e857d320dad4bddf6988a4442688908d39871
Cuyler36 <jeremy96@vt.edu>
2018-08-06 22:35:21 -0400

    Remove unused class & file

--- Custom Villager Creator/ColorExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿// ReSharper disable once CheckNamespace
-namespace System.Windows.Media
-{
-    public static class ColorExtensions
-    {
-        public static Color FromArgb(this Color c, int argb) => Color.FromArgb((byte) (argb >> 24), (byte) (argb >> 16), (byte) (argb >> 8), (byte) argb);
-        public static Color FromArgb(this Color c, uint argb) => FromArgb(c, (int) argb);
-    }
-}
